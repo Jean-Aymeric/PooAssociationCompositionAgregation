894c98df8b1ca065b2a32a3395687a6ee419fb15
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-10-02 11:52:25 +0200

    add corge

--- corge.py
+++ corge.py
@@ -1,2 +1,13 @@
+from foo import Foo
+
+
 class Corge:
-    ...
+    __foo: Foo
+
+    def __init__(self, foo: Foo):
+        self.__foo = foo
+
+    def setFoo(self, foo: Foo):
+        self.__foo = foo
+        if self.__foo.getCorge() != self:
+            self.__foo.setCorge(self)
--- foo.py
+++ foo.py
@@ -1,17 +1,19 @@
 from bar import Bar
 from baz import Baz
 from qux import Qux
-
+from corge import Corge
 
 class Foo:
     __bar: Bar
     __bazs: [Baz]
     __qux: Qux
+    __corge: Corge
 
     def __init__(self, bar: Bar):
         self.__bar = bar
         self.__bazs = []
         self.__qux = Qux()
+        self.__corge = None
 
     def getBar(self) -> Bar:
         return self.__bar
@@ -36,3 +38,10 @@ class Foo:
 
     def getQux(self) -> Qux:
         return self.__qux
+
+    def setCorge(self, corge: Corge):
+        self.__corge = corge
+        self.__corge.setFoo(self)
+
+    def getCorge(self) -> Corge:
+        return self.__corge
